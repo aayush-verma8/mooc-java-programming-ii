2fa528f0cfbb288f33f317d765c20ffae001f11a
aayush-verma8 <aayushverma.av2025@gmail.com>
2026-03-29 21:14:35 +0530

    "Part08_10 Completed"

---
+++ part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,27 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aayus
+ */
+public class IOU {
+    private HashMap<String, Double> IOU;
+    public IOU(){
+        this.IOU=new HashMap<>();
+    }
+    public void setSum(String toWhom, double amount){
+        
+        this.IOU.put(toWhom, amount);
+    }
+    public double howMuchDoIOweTo(String toWhom){
+        return this.IOU.getOrDefault(toWhom, 0.0);
+    }
+    
+}
