83fbcaccf1c088b54bdf9756513001f0655f3c81
aayush-verma8 <aayushverma.av2025@gmail.com>
2026-04-20 11:47:40 +0530

    Part09_5 Completed

---
+++ part09-Part09_05.TacoBoxes/src/main/java/CustomTacoBox.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aayus
+ */
+public class CustomTacoBox extends TripleTacoBox{
+    private int tacos;
+    
+    public CustomTacoBox(int tacos){
+        super.setTacos(tacos);
+    }
+}
